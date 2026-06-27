27459cc3080f68b368c410085e4e4ee9260f77c7
ChiYann <45175688+ChiyannQ@users.noreply.github.com>
2026-06-27 17:43:37 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,72 +3,74 @@
 #
 #  demo.py
 #  
-#  Copyright 2021 PMT <PMT@PN04>
- 
+#  Copyright 2021. Ian
+#  
+
+
+#from multiprocessing import freeze_support
+
+
+
 
 import fire
-from multiprocessing import freeze_support
+import os 
+
+# data process
+import torch
+import torch.nn.functional as F
+import cv2
+import numpy as np
+
 
 import apc.base as base
 import apc.dcmIO as dIO
-import apc.seg_wV as sg
+import apc.seg_ct as sgCT
+import apc.seg_mr as sgMR
 import apc.imgProc as imgP
+import utils.masktocontours as masktocontours
 
 
-# data process
-import cv2
-import numpy as np
-import torch
-import torch.nn.functional as F
 
-import os
+
 #import pydicom
 import SimpleITK as sitk
 import csv
 
 
+
 def loadP3D(pdir):
+    
     
     reader = sitk.ImageSeriesReader()
     reader.MetaDataDictionaryArrayUpdateOn()
     dicom_names = reader.GetGDCMSeriesFileNames(pdir)
     reader.SetFileNames(dicom_names)
     image = reader.Execute()
-    img_array = sitk.GetArrayFromImage(image)  #frame_num, width, height = img_array.shape
+    img_array = sitk.GetArrayFromImage(image)
+    
+    mod = reader.GetMetaData(0,'0008|0060').lower()
     
     x0,y0,z0 = image.GetOrigin()
     dx,dy,dz = image.GetSpacing()
-    print(img_array.dtype) #  int16,uint16,float32,
-    #vol = img_array.astype(np.uint16) # (65535//img_array.max())
     
-    vol = base.stdVol16(img_array) # 对每一层std
-    vol_std = base.stdImg(img_array)  # 对整个std
-    
-    return vol,vol_std,(x0,y0,z0),(dx,dy,dz)
-    
-def complexFilter(vol_e):
-    
-    # aug
-    vol_eAug = imgP.augMRI(vol_e)
-    filterApc = imgP.getFilter()
-    
-    # filter
-    apc3D_e = imgP.apcFilter(vol_eAug,filterApc)
-    lap_uint8_e = imgP.lapFilter(vol_eAug)
-    
-    
-    # crop extra_edge
-    ext = 10
-    apcV = apc3D_e[:,ext:-ext,ext:-ext]
-    lap_V = lap_uint8_e[:,ext:-ext,ext:-ext]
-    
-    return vol_eAug,apcV,lap_V
-    
-def vol_crop(vol,mri3D):
-    center = dIO.getCenter(vol)
-    bbox,bbox_e = dIO.getBBox(center)
-    mri3D_e = dIO.cutVol(mri3D,bbox_e)
-    return mri3D_e,bbox
+    if 'c' in mod:
+        #TODO
+        print('preparing the CT dcm data ...')
+        img_array[img_array<-1000] = -1000
+        min_bound = img_array.min()
+        img_array += abs(min_bound)
+        img_array[img_array<0] = 0 # 这里的min，max都是全局的不是对每个slice单独搞
+        mx = img_array.max()
+        vol_std = img_array.astype(np.float32)/mx*255
+        #print(vol_std.max(),vol_std.min())
+        return vol_std.astype(np.uint8),(x0,y0,z0),(dx,dy,dz),'ct'
+        
+    elif 'm' in mod:
+        print('preparing the MRI dcm data ...') 
+        #vol = base.stdVol16(img_array) # 这个有必要吗?? 
+        vol_std = base.stdVol(img_array)
+        
+        return vol_std,(x0,y0,z0),(dx,dy,dz),'mr'
 
 def outApcVis(root,contours,vol):
     newPdir = base.makeNewPath(root,'result','apcVis')
@@ -117,87 +119,105 @@ def saveRes(root,cls,bbox,origin,spacing,h):
     
     outPath = base.makeNewPath(root,'result','phyCoor')
     for idx,c in enumerate(cls):
+        open(os.path.join(outPath, 'coord_%d.csv' % (idx)), 'w', newline='')
         for seed in c:
-
             lyIdx,x_tmp,y_tmp = seed
             x_tmp += x_t
             y_tmp += y_l
-
             #
-            center_phy = [z0+dz*lyIdx, x0+x_tmp*dx, y0+y_tmp*(h-1-dy)]
+            center_phy = [x0+x_tmp*dx, y0+dy*(h-1-y_tmp), z0+dz*lyIdx]
             #
             with open(os.path.join(outPath,'coord_%d.csv'%(idx)),'a',newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(center_phy)
-                
+
+def getAppContours(cls,bbox):
+    x_t, y_l = bbox[2], bbox[0]
+    appcontour = []
+    for idx, c in enumerate(cls):
+        cls_pixel = []
+        for seed in c:
+            lyIdx, x_tmp, y_tmp = seed
+            x_tmp += x_t
+            y_tmp += y_l
+            #
+            center_pixel = [lyIdx, y_tmp, x_tmp]
+            #
+            cls_pixel.append(np.array(center_pixel))
+        appcontour.append(cls_pixel)
+    return appcontour
+    
+def vol_crop(vol,radius):
+    center = dIO.getCenter(vol)
+    bbox,bbox_e = dIO.getBBox(center,radius)
+    mri3D_e = dIO.cutVol(vol,bbox_e)
+    return mri3D_e,bbox
+    
+    
+def seedsInCT(mri3D_e,ext):
+    ext = 10
+    filterApc = imgP.getFilter()
+    apc3D_e = imgP.apcFilter(mri3D_e,filterApc)
+    apc3De_bin = (apc3D_e>235).astype(np.uint8)
+    
+    #==========3D aga======================
+    seedVol = apc3De_bin[ :,ext:-ext,ext:-ext]
+    vol = mri3D_e[:, ext:-ext,ext:-ext]
+    
+    return seedVol,vol
 
 
-
-def apcLoc(srcdir,inpType='dicom',rg=None,cuda=0,
-          repoRoot=None,gThr = 70,k=14,debug=False):
+def apcLoc(srcdir,inpType='dicom',rg=None,cuda=0,  #gradThr = 50,gray_min = 70,k=8,
+          repoRoot=None,
+          debug=False):
     '''
     args:
         -debug: if True, vis the contour results
-    
+    apcLoc --srcdir path2yourdata
     
     '''
     
     repoRoot = os.path.dirname(os.path.realpath(__file__))
     
     #if inpType = 'dicom':
-    mri3D,mri3D_u8,origin,spacing = loadP3D(srcdir)
-    
-    # crop
-    mri3D_e, bbox= vol_crop(mri3D_u8,mri3D)# dIO.cutVol(mri3D,bbox_e)
-    
-    #mri3D_eAug = imgP.augMRI(mri3D_e)# uint16
-    mri3D_eAug,apc3D , lap_uint8 = complexFilter(mri3D_e)
+    vol_u8,origin,spacing,mod = loadP3D(srcdir)
     ext = 10
     
     
-    # ====================================================
-    k_values = imgP.topK(apc3D,k) #N,1 
-    apc3D_binary = imgP.bin3D(apc3D,k_values)   #二值化  uint8
-    filling3D_binary = imgP.drawHole(lap_uint8)  # uint8
-    dihs =  imgP.overlap(apc3D_binary,filling3D_binary) # dots in holes uint8 
     
-    #===============================================================
-    vol = mri3D_eAug[ :,ext:-ext,ext:-ext]
-    vol = base.stdVol(vol)
+    #==============find the seeds=================
+    if mod=='ct':
+        mri3D_e,bbox = vol_crop(vol_u8,60)
+        gradThr = 50
+        gray_min  = 70   #gray_min = 70 # 最小灰度值   thr_grad = 50
+        seedVol,vol = seedsInCT(mri3D_e,ext) #apc3De_bin[ :,ext:-ext,ext:-ext]  vol = mri3D_e[:, ext:-ext,ext:-ext]
     
-    
+    #==========3D aga======================
     contours = []
-    growed = []
+    growed = []         
     
-    rg_thr = 48 # region growing 最小值，低于这个值就不grow
-    
-    depth,h,w = mri3D.shape
-    
-    
-    for idx in range(depth): # visit all the slice
-        dih = dihs[idx]
-        
-        if dih.max() == 0: # 如果没有candidates，那就下一层
+    depth,h,w = vol.shape
+    for idx in range(depth): 
+        seedPlane = seedVol[idx]
+
+        if seedPlane.max() == 0: # 如果没有candidates，那就下一层
             continue
-        seeds = sg.makeSeeds(dih,idx) # For every slice, return some candidate seeds
-        for seed in seeds: # loop over seeds
-            
-            # if seed had been visited or below the threshold; skip
-            if sg.is_inMarked(seed,growed) or vol[seed[0],seed[1],seed[2]]<=rg_thr:
+        seeds = sg.makeSeeds(seedPlane,idx) #
+        for seed in seeds:
+
+            if sg.is_inMarked(seed,growed) or vol[seed[0],seed[1],seed[2]]<=gray_min:
                 continue
-            # grow
-            mrked3D, markedSet = sg.regionGrow3D(vol,apc3D,seed,thr=rg_thr,grad_thr = gThr)
-            
-            if mrked3D is None:# if no return, next
+            mrked3D, markedSet = sg.regionGrow3D(vol,seed,thr=gray_min,grad_thr = gradThr)
+            if mrked3D is None:
                 continue
-            
-            # else: save
-            contours.append(mrked3D)# 
+            contours.append(mrked3D)
             growed.append(markedSet)
-        
-        
+    
+    
+    
     #===============================================================
     if debug:
+        
         outApcVis(repoRoot,contours,vol)
         
     cls = getCls(contours)
@@ -206,12 +226,15 @@ def apcLoc(srcdir,inpType='dicom',rg=None,cuda=0,
     
     #===============================================================
     saveRes(repoRoot,cls,bbox,origin,spacing,h)
-    
+    #=======from wen============================
+    appcontour = getAppContours(cls,bbox)
+    masktocontours.ApptoContours(appcontour,srcdir , srcdir)
+
     return 0
     
 
 if __name__ == '__main__':
     #import sys
     #sys.exit(main(sys.argv))
-    freeze_support()#https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+    #freeze_support()#https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
     fire.Fire(apcLoc)
